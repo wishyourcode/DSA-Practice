8fafda001bccb02447b2396858e0f7c4e9e9923f
wishyourcode <vishalm3101@gmail.com>
2025-12-18 00:29:44 +0530

    learned right side aligned pattern

---
+++ pattern/ArrowRightAlign.java
@@ -0,0 +1,25 @@
+package pattern;
+
+public class ArrowRightAlign {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int space = 1; space <= (n - i); space++) { // 5-0 =5
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int space = 1; space <= (n - i); space++) { // 5-0 =5
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ pattern/reverseRightAlign.java
@@ -0,0 +1,14 @@
+public class reverseRightAlign {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = n; i >= 1; i--) {
+            for (int space = 1; space <= (n - i); space++) { // 5-0 =5
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
