1c725acfa84e2aab76354ca93251eee4e92dcd24
wishyourcode <vishalm3101@gmail.com>
2025-12-21 20:50:31 +0530

    using cyclic sort find missing number in a 0 to N array

---
+++ leetcode/missingnum.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+public class missingnum {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4 };
+        int result = missingnumber(arr);
+        System.out.println(result);
+    }
+
+    static int missingnumber(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctindex = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correctindex]) {
+                swap(arr, i, correctindex);
+            } else {
+                i++;
+            }
+        }
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index) {
+                return index;
+            }
+        }
+        return arr.length;
+    }
+
+    static void swap(int[] arr, int value, int correctindex) {
+        int temp = arr[value];
+        arr[value] = arr[correctindex];
+        arr[correctindex] = temp;
+    }
+}
--- sorting/cyclicsort.java
+++ sorting/cyclicsort.java
@@ -1,6 +1,8 @@
+package sorting;
+
 import java.util.Arrays;
 
-public class syclicsort {
+public class cyclicsort {
     public static void main(String[] args) {
         int[] arr = { 5, 4, 3, 2, 1 };
         cyclic(arr);
