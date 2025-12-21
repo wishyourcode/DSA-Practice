d9c3629c073d5a4bc6c3471b5721ea9c1d05a135
wishyourcode <vishalm3101@gmail.com>
2025-12-21 20:18:44 +0530

    learn cyclic sort

---
+++ syclicsort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class syclicsort {
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 3, 2, 1 };
+        cyclic(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cyclic(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int correctindex = arr[i] - 1;
+            if (arr[i] != arr[correctindex]) {
+                swap(arr, i, correctindex);
+            }
+        }
+    }
+
+    static void swap(int[] arr, int value, int correctindex) {
+        int temp = arr[value];
+        arr[value] = arr[correctindex];
+        arr[correctindex] = temp;
+    }
+}
