eecea680e9eda9036e9bf332d9babf1b3439c481
wishyourcode <vishalm3101@gmail.com>
2025-12-24 21:28:54 +0530

    optimal solution of the find duplicates elements

---
+++ Striver_Sheet/arrays/O_Remove_Duplicates.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class O_Remove_Duplicates {
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 2, 2, 3, 3, 4, 4 };
+        System.out.println(Arrays.toString(arr));
+        int n = removeDuplicate(arr);
+        System.out.println("n = " + n);
+        System.out.print("Array after removing duplicates: ");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static int removeDuplicate(int[] arr) {
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[j] != arr[i]) {
+                arr[i + 1] = arr[j];
+                i++;
+            }
+        }
+        return i + 1;
+    }
+}
