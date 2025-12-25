e95746ebfef2ae2e3eb68f768ee193c125ca95be
wishyourcode <vishalm3101@gmail.com>
2025-12-25 23:01:08 +0530

    learn to how can find the intersection elements  of the arrays

--- Striver_Sheet/arrays/O_Union_Array.java
+++ Striver_Sheet/arrays/O_Union_Array.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class O_Union_Array {
     public static void main(String[] args) {
-        int[] arr1 = { 1, 2, 3, 4, 5, 6, 7 };
+        int[] arr1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int[] arr2 = { 2, 3, 5, 6, 7, 8, 9 };
         int n = arr1.length;
         int m = arr2.length;
@@ -11,20 +11,44 @@ public class O_Union_Array {
     }
 
     public static ArrayList<Integer> union(int[] arr1, int[] arr2, int n, int m) {
-        ArrayList<Integer> unionarray = new ArrayList<>();
-        int i = 0;
-        int j = 0;
+        ArrayList<Integer> unionArray = new ArrayList<>();
+        int i = 0, j = 0;
+
         while (i < n && j < m) {
-            if (arr1[i] < arr2[j]) {
+            if (arr1[i] == arr2[j]) {
+                if (unionArray.size() == 0 || unionArray.get(unionArray.size() - 1) != arr1[i]) {
+                    unionArray.add(arr1[i]);
+                }
                 i++;
-            } else if (arr1[i] > arr2[j]) {
                 j++;
-            } else {
-                unionarray.add(arr1[i]);
+            } else if (arr1[i] < arr2[j]) {
+                if (unionArray.size() == 0 || unionArray.get(unionArray.size() - 1) != arr1[i]) {
+                    unionArray.add(arr1[i]);
+                }
                 i++;
+            } else {
+                if (unionArray.size() == 0 || unionArray.get(unionArray.size() - 1) != arr2[j]) {
+                    unionArray.add(arr2[j]);
+                }
                 j++;
             }
         }
-        return unionarray;
+
+        while (i < n) {
+            if (unionArray.get(unionArray.size() - 1) != arr1[i]) {
+                unionArray.add(arr1[i]);
+            }
+            i++;
+        }
+
+        while (j < m) {
+            if (unionArray.get(unionArray.size() - 1) != arr2[j]) {
+                unionArray.add(arr2[j]);
+            }
+            j++;
+        }
+
+        return unionArray;
     }
+
 }
