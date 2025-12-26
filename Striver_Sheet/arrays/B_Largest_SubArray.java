bb8240d55c8ac31eecf744ba27b11d94541b5e82
wishyourcode <vishalm3101@gmail.com>
2025-12-26 20:29:46 +0530

    find the largest subarray with given sum k

---
+++ Striver_Sheet/arrays/B_Largest_SubArray.java
@@ -0,0 +1,24 @@
+public class B_Largest_SubArray {
+    public static void main(String[] args) {
+        int[] nums = { -3, 2, 1 };
+        int k = 6;
+        int ans = Largest_SubArray_sum(nums, k);
+        System.out.println(ans);
+    }
+
+    public static int Largest_SubArray_sum(int[] arr, int k) {
+        int leng = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                int temp = 0;
+                if (sum == k) {
+                    temp = j - i + 1;
+                    leng = Math.max(temp, leng);
+                }
+            }
+        }
+        return leng;
+    }
+}
