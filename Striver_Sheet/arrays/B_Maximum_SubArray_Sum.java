698c01ad04a392189148c5fdbf432fa333f184ed
wishyourcode <vishalm3101@gmail.com>
2025-12-27 20:15:36 +0530

    find maximum subarray sum in an array

---
+++ Striver_Sheet/arrays/B_Maximum_SubArray_Sum.java
@@ -0,0 +1,19 @@
+public class B_Maximum_SubArray_Sum {
+    public static void main(String[] args) {
+        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int ans = MaxSubArray(nums);
+        System.out.println(ans);
+    }
+
+    public static int MaxSubArray(int[] arr) {
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                max = Math.max(max, sum);
+            }
+        }
+        return max;
+    }
+}
