05202fdb51f30cf9b8742806a2d4d3b9d14fc668
wishyourcode <vishalm3101@gmail.com>
2025-12-28 16:08:07 +0530

    find leader elements in an array

---
+++ Striver_Sheet/arrays/B_Leader_Element.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class B_Leader_Element {
+    public static void main(String[] args) {
+        int[] arr = { 10, 12, 8, 9, 6 };
+        ArrayList<Integer> result = Find_Learder(arr);
+        System.out.println(result);
+    }
+
+    public static ArrayList<Integer> Find_Learder(int[] arr) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            boolean leader = true;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > arr[i]) {
+                    leader = false;
+                    break;
+                }
+            }
+            if (leader == true) {
+                ans.add(arr[i]);
+            }
+        }
+        return ans;
+    }
+}
