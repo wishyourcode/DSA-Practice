55d57f625e9e0b33ad927fb7eebc6e93853d79b5
wishyourcode <vishalm3101@gmail.com>
2025-12-28 12:22:19 +0530

    Optimal Solution for

---
+++ Striver_Sheet/arrays/O_RearrangingArray.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class O_RearrangingArray {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, -4, -5 };
+        ArrangeArray(arr);
+
+    }
+
+    public static void ArrangeArray(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        int posIndex = 0;
+        int negIndex = 1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) {
+                ans[posIndex] = arr[i];
+                posIndex += 2;
+            } else {
+                ans[negIndex] = arr[i];
+                negIndex += 2;
+            }
+        }
+        System.out.println(Arrays.toString(ans));
+
+    }
+}
