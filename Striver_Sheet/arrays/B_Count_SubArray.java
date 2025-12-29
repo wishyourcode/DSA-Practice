e1b334cf9bcbd88feb2a1770f0ce6038a71195e1
wishyourcode <vishalm3101@gmail.com>
2025-12-29 23:47:24 +0530

    better way to count the sub array with given sum

---
+++ Striver_Sheet/arrays/B_Count_SubArray.java
@@ -0,0 +1,21 @@
+public class B_Count_SubArray {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, -3, 1, 1, 1, 4, 2, -3 };
+        int ans = countSubArray(arr, 3);
+        System.out.println(ans);
+    }
+
+    public static int countSubArray(int[] arr, int k) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
