a430b4077ef2874aad38ba90f85f5902af4db9a2
wishyourcode <vishalm3101@gmail.com>
2025-12-30 13:05:03 +0530

    find the insertion index for the element if element already exit then return the index of the element

---
+++ Striver_Sheet/Binary_Search/O_Insert_Position.java
@@ -0,0 +1,27 @@
+package Striver_Sheet.Binary_Search;
+
+public class O_Insert_Position {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 4, 7 };
+        int ans = InsertPosition(arr, 6);
+        System.out.println(ans);
+    }
+
+    public static int InsertPosition(int[] arr, int target) {
+
+        int ans = arr.length;
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start);
+            if (arr[mid] > target) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+}
