b30817fd15a013c20ccfa6acf08c028d4d1fcebd
wishyourcode <vishalm3101@gmail.com>
2025-12-30 12:50:55 +0530

    find the upper bound eleents index in a array

---
+++ Striver_Sheet/Binary_Search/O_upperBound.java
@@ -0,0 +1,25 @@
+package Striver_Sheet.Binary_Search;
+
+public class O_upperBound {
+    public static void main(String[] args) {
+        int[] arr = { 3, 5, 8, 15, 19 };
+        int ans = upperBound(arr, 15);
+        System.out.println(ans);
+    }
+
+    public static int upperBound(int[] arr, int target) {
+        int ans = arr.length;
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start);
+            if (arr[mid] > target) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
