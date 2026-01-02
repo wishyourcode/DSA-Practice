27da37eaa124d12f605c9890c1a289de2c30ce17
wishyourcode <vishalm3101@gmail.com>
2026-01-02 23:37:21 +0530

    find digital sum

---
+++ basic_array/digitalsum.java
@@ -0,0 +1,23 @@
+package basic_array;
+
+import java.util.Scanner;
+
+public class digitalsum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int ans = digitsum(n);
+        System.out.println(ans);
+        input.close();
+    }
+
+    public static int digitsum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            sum += rem;
+            n = n / 10;
+        }
+        return sum;
+    }
+}
