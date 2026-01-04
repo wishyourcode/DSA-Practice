915a08a2e4ea914e5f80d948ce3ae319b5643867
wishyourcode <vishalm3101@gmail.com>
2026-01-04 23:13:10 +0530

    print the array in reverse

---
+++ logic/Print_IN_Reverse.java
@@ -0,0 +1,14 @@
+package logic;
+
+public class Print_IN_Reverse {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Reverse(arr);
+    }
+
+    public static void Reverse(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
