5999480597e2c7435f945b8980f5a23a08428d90
wishyourcode <vishalm3101@gmail.com>
2026-01-04 22:36:58 +0530

    find  Unique Element in array

---
+++ CodingNinja/uniqueElement.java
@@ -0,0 +1,17 @@
+package CodingNinja;
+
+public class uniqueElement {
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 1, 6, 3, 6, 2 };
+        int ans = findUnique(arr);
+        System.out.println(ans);
+    }
+
+    public static int findUnique(int[] arr) {
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {// 1122334
+            ans ^= arr[i];
+        }
+        return ans;
+    }
+}
