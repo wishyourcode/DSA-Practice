d3eb947c03b71a11aea896691d5727201d6563ae
wishyourcode <vishalm3101@gmail.com>
2026-01-06 01:23:57 +0530

    transpose of the matrix

--- logic/TranposeMatrix.java
+++ logic/TranposeMatrix.java
@@ -5,19 +5,21 @@ public class TranposeMatrix {
         int[][] arr1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
         int[][] arr2 = { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
         transpose(arr1);
+        System.out.println("=========================");
+        transpose(arr2);
     }
 
-    public static void transpose(int[][] arr1) {
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = i + 1; j < arr1.length; j++) {
-                int temp = arr1[i][j];
-                arr1[i][j] = arr1[j][i];
-                arr1[j][i] = temp;
+    public static void transpose(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
             }
         }
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr1.length; j++) {
-                System.out.print(arr1[i][j] + " ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
