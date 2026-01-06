6f5b6161cec0bc1fa4cf010c054a9c913fc80a91
wishyourcode <vishalm3101@gmail.com>
2026-01-06 22:30:07 +0530

    added details of the Q

--- CodingNinja/findduplicate.java
+++ CodingNinja/findduplicate.java
@@ -14,6 +14,15 @@ class findduplicate {
         System.out.println(result);
     }
 
+    /*
+     * You are given an array ‘ARR’ of size ‘N’ containing each number between 1 and
+     * ‘N’ - 1 at least once. There is a single integer value that is present in the
+     * array twice. Your task is to find the duplicate integer value present in the
+     * array.
+     * For example: Consider ARR = [1, 2, 3, 4, 4], the duplicate integer value
+     * present in the array is 4. Hence, the answer is 4 in this case.
+     * Note : A duplicate number is always present in the given array.
+     */
     public static int findDuplicate(ArrayList<Integer> arr) {
         ArrayList<Integer> seen = new ArrayList<>();
         for (int i : arr) {
