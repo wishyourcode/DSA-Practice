29300b72bf54b6467c4b27a46bc4471e4376626b
wishyourcode <vishalm3101@gmail.com>
2026-01-12 23:59:44 +0530

    f

---
+++ CodingNinja/AddString.java
@@ -0,0 +1,18 @@
+public class AddString {
+    /*
+     * Problem statement
+     * You are given two non-negative integers, ‘NUM1’ and ‘NUM2’, in the form of
+     * strings. Return the sum of both strings.
+     * Note :
+     * You do not need to print anything, it has already been taken care of. Just
+     * implement the given function.
+     * Example:
+     * Let ‘NUM1’ be: “5”
+     * Let ‘NUM2’ be: “21”
+     * The sum of both numbers will be: “26”.
+     */
+    long a = Long.parseLong(num1);
+    long b = Long.parseLong(num2);
+    long sum = a + b;return Long.toString(sum);
+
+}
