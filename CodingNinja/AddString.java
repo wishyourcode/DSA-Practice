cc19a7219f219dba04a4031f6c549bfca7a37f2d
wishyourcode <vishalm3101@gmail.com>
2026-01-13 00:03:18 +0530

    ninja coding DSA Q solved

--- CodingNinja/AddString.java
+++ CodingNinja/AddString.java
@@ -11,8 +11,16 @@ public class AddString {
      * Let ‘NUM2’ be: “21”
      * The sum of both numbers will be: “26”.
      */
-    long a = Long.parseLong(num1);
-    long b = Long.parseLong(num2);
-    long sum = a + b;return Long.toString(sum);
+    public static String stringConcatenation(String num1, String num2) {
+        long a = Long.parseLong(num1);
+        long b = Long.parseLong(num2);
+        long sum = a + b;
+        return Long.toString(sum);
+    }
 
+    public static void main(String[] args) {
+        String num1 = "5";
+        String num2 = "21";
+        System.out.println(stringConcatenation(num1, num2));
+    }
 }
