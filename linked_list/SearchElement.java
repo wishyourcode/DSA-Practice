5b591543b77ae462d2b26dbe00860d29708b93a9
wishyourcode <vishalm3101@gmail.com>
2026-01-13 22:49:51 +0530

    Start learning linked list

---
+++ linked_list/SearchElement.java
@@ -0,0 +1,47 @@
+package linked_list;
+
+class Node {
+    public int data;
+    public Node next;
+
+    Node() {
+        this.data = 0;
+        this.next = null;
+    }
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+public class SearchElement {
+    public static int searchInLinkedList(Node head, int k) {
+        Node temp = head;
+
+        while (temp != null) {
+            if (temp.data == k) {
+                return 1; // Found
+            }
+            temp = temp.next;
+        }
+
+        return 0; // Not found
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(3);
+        head.next = new Node(5);
+        head.next.next = new Node(7);
+        head.next.next.next = new Node(9);
+
+        int k = 17;
+        int result = searchInLinkedList(head, k);
+        System.out.println("Element " + k + (result == 1 ? " found." : " not found."));
+    }
+}
