cd869680b77332b3dcbedaa38e6e2175754742c8
wishyourcode <vishalm3101@gmail.com>
2026-01-18 22:02:46 +0530

    Q explanation

--- CodingNinja/Search_in_Rotating_Array.java
+++ CodingNinja/Search_in_Rotating_Array.java
@@ -1,4 +1,19 @@
 public class Search_in_Rotating_Array {
+    /*
+     * Aahad and Harshit always have fun by solving problems. Harshit took a sorted
+     * array consisting of distinct integers and rotated it clockwise by an unknown
+     * amount. For example, he took a sorted array = [1, 2, 3, 4, 5] and if he
+     * rotates it by 2, then the array becomes: [4, 5, 1, 2, 3].
+     * 
+     * After rotating a sorted array, Aahad needs to answer Q queries asked by
+     * Harshit, each of them is described by one integer Q[i]. which Harshit wanted
+     * him to search in the array. For each query, if he found it, he had to shout
+     * the index of the number, otherwise, he had to shout -1.
+     * 
+     * For each query, you have to complete the given method where 'key' denotes
+     * Q[i]. If the key exists in the array, return the index of the 'key',
+     * otherwise, return -1.
+     */
     public static int search(int arr[], int key) {
         int low = 0;
         int high = arr.length - 1;
