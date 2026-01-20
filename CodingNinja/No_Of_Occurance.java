b0ca33f0b1d1ac622b9750acf6651a5d70ef4602
wishyourcode <vishalm3101@gmail.com>
2026-01-20 23:56:18 +0530

    occurance count

--- CodingNinja/No_Of_Occurance.java
+++ CodingNinja/No_Of_Occurance.java
@@ -1,7 +1,17 @@
 public class No_Of_Occurance {
+    public static int count(int arr[], int n, int x) {
+        int first = first(arr, n, x);
+        if (first == -1) {
+            return 0;
+        }
+        int last = last(arr, n, x);
+        int ans = last - first + 1;
+        return ans;
+    }
+
     public static int first(int[] arr, int n, int x) {
         int low = 0;
-        int high = n - 1;
+        int high = n;
         int ans = -1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
@@ -39,7 +49,11 @@ public class No_Of_Occurance {
 
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 5, 5, 6 };
-        int first = first(arr, 8, 6);
-        System.out.println(first);
+        int first = first(arr, 8, 5);
+        System.out.println("first : " + first);
+        int last = last(arr, 8, 5);
+        System.out.println("last : " + last);
+        int ans = count(arr, 8, 5);
+        System.out.println("count : " + ans);
     }
 }
