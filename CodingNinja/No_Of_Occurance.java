0b18406129185886148a28c879c80995f070beaa
wishyourcode <vishalm3101@gmail.com>
2026-01-21 00:07:57 +0530

    solved

--- CodingNinja/No_Of_Occurance.java
+++ CodingNinja/No_Of_Occurance.java
@@ -1,25 +1,26 @@
 public class No_Of_Occurance {
+
     public static int count(int arr[], int n, int x) {
         int first = first(arr, n, x);
         if (first == -1) {
             return 0;
         }
         int last = last(arr, n, x);
-        int ans = last - first + 1;
-        return ans;
+        return last - first + 1;
     }
 
     public static int first(int[] arr, int n, int x) {
         int low = 0;
-        int high = n;
+        int high = n - 1;
         int ans = -1;
+
         while (low <= high) {
             int mid = low + (high - low) / 2;
+
             if (arr[mid] == x) {
                 ans = mid;
                 high = mid - 1;
-            }
-            if (x < arr[mid]) {
+            } else if (x < arr[mid]) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
@@ -32,13 +33,14 @@ public class No_Of_Occurance {
         int low = 0;
         int high = n - 1;
         int ans = -1;
+
         while (low <= high) {
             int mid = low + (high - low) / 2;
+
             if (arr[mid] == x) {
                 ans = mid;
                 low = mid + 1;
-            }
-            if (x < arr[mid]) {
+            } else if (x < arr[mid]) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
@@ -48,12 +50,15 @@ public class No_Of_Occurance {
     }
 
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5, 5, 5, 6 };
-        int first = first(arr, 8, 5);
+        int[] arr = { 1, 2, 4, 4, 5 };
+
+        int first = first(arr, arr.length, 4);
         System.out.println("first : " + first);
-        int last = last(arr, 8, 5);
+
+        int last = last(arr, arr.length, 4);
         System.out.println("last : " + last);
-        int ans = count(arr, 8, 5);
+
+        int ans = count(arr, arr.length, 4);
         System.out.println("count : " + ans);
     }
 }
