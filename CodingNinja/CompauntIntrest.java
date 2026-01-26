2f3fd42000bd78df3834fedc936eee5c88a26682
wishyourcode <vishalm3101@gmail.com>
2026-01-26 22:58:59 +0530

    ninja coding dsa q

---
+++ CodingNinja/CompauntIntrest.java
@@ -0,0 +1,22 @@
+public class CompauntIntrest {
+    /*
+     * Create a Compound Interest program that calculates the total amount
+     * accumulated after a certain period based on the following attributes:
+     * 
+     * a. principalAmount: The principal value deposited
+     * 
+     * b. annualInterestRate: Interest rate per year(expressed as a decimal).
+     * 
+     * c. numberOfYears: Period for which interest has to be given.
+     * 
+     * 
+     * Requirements:
+     * 
+     * 1. Calculate the total amount accumulated using the compound interest
+     * formula.
+     * 2. Do not use toFixed or round off the answer.
+     * 3. Use a while loop to calculate the total amount over the specified number
+     * of years.
+     * 4. Store the final amount in the answer variable.
+     */
+}
