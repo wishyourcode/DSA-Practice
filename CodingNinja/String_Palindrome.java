26ad5225b70aa137b6b7760e8b0651274e01acde
wishyourcode <vishalm3101@gmail.com>
2026-01-26 23:44:22 +0530

    palindrome in string

---
+++ CodingNinja/String_Palindrome.java
@@ -0,0 +1,33 @@
+public class String_Palindrome {
+    /*
+     * You are given a string 'S'. Your task is to check whether the string is
+     * palindrome or not. For checking palindrome, consider alphabets and numbers
+     * only and ignore the symbols and whitespaces.
+     * 
+     * Note :
+     * 
+     * String 'S' is NOT case sensitive.
+     * Example :
+     * 
+     * Let S = “c1 O$d@eeD o1c”.
+     * If we ignore the special characters, whitespaces and convert all uppercase
+     * letters to lowercase, we get S = “c1odeedo1c”, which is a palindrome. Hence,
+     * the given string is also a palindrome.
+     */
+
+    public static boolean isPalindrome(String S) {
+        // Convert to lowercase and remove non-alphanumeric characters
+        String filtered = S.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        // Check if the filtered string is equal to its reverse
+        int left = 0, right = filtered.length() - 1;
+        while (left < right) {
+            if (filtered.charAt(left) != filtered.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
