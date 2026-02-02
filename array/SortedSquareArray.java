5787365d8a9ecdd76204b7e102ca101a1509e73b
wishyourcode <vishalm3101@gmail.com>
2026-02-02 23:49:55 +0530

    sorting the ans

--- array/SortedSquareArray.java
+++ array/SortedSquareArray.java
@@ -7,6 +7,14 @@ public class SortedSquareArray {
     public static int[] sortedSquare(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             nums[i] = nums[i] * nums[i];
+            while (nums[i] < nums[i - 1]) {
+                int temp = nums[i];
+                nums[i] = nums[i - 1];
+                nums[i - 1] = temp;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+
         }
         return nums;
     }
