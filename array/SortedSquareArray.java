f18b88fb1dff36f9a45aadbd5437543f61872b0c
wishyourcode <vishalm3101@gmail.com>
2026-02-02 23:59:36 +0530

    Sorted array using insertion sort

--- array/SortedSquareArray.java
+++ array/SortedSquareArray.java
@@ -7,20 +7,20 @@ public class SortedSquareArray {
     public static int[] sortedSquare(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             nums[i] = nums[i] * nums[i];
-            while (nums[i] < nums[i - 1]) {
-                int temp = nums[i];
-                nums[i] = nums[i - 1];
-                nums[i - 1] = temp;
-            }
-        }
-        for (int i = 0; i < nums.length; i++) {
 
+            int j = i;
+            while (j > 0 && nums[j] < nums[j - 1]) {
+                int temp = nums[j];
+                nums[j] = nums[j - 1];
+                nums[j - 1] = temp;
+                j--;
+            }
         }
         return nums;
     }
 
     public static void main(String[] args) {
-        int[] nums = { -4, -1, 0, 3, 10 };
+        int[] nums = { -4, -1, 2, 0, 10 };
         int[] ans = sortedSquare(nums);
         System.out.println(Arrays.toString(ans));
     }
