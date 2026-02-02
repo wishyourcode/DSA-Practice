83d20fc95f83f5cc587b2f2e97bb80ca30d05642
wishyourcode <vishalm3101@gmail.com>
2026-02-02 23:39:14 +0530

    Number with even digit

--- array/Number_with_Even_Digit.java
+++ array/Number_with_Even_Digit.java
@@ -5,6 +5,23 @@ package array;
 public class Number_with_Even_Digit {
     public static int findNumbers(int[] arr) {
         int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            int num = arr[i];
+            while (num != 0) {
+                num = num / 10;
+                sum += 1;
+            }
+            if (sum % 2 == 0) {
+                count++;
+            }
+        }
         return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 12, 345, 21, 6, 7896 };
+        int ans = findNumbers(arr);
+        System.out.println(ans);
     }
 }
