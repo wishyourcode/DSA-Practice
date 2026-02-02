632c0f1ed93d756472dad996ba0a4a65948f5b25
wishyourcode <vishalm3101@gmail.com>
2026-02-02 23:31:16 +0530

    find number with even Number of Digit

---
+++ array/Number_with_Even_Digit.java
@@ -0,0 +1,10 @@
+package array;
+
+// https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
+/*Given an array nums of integers, return how many of them contain an even number of digits.*/
+public class Number_with_Even_Digit {
+    public static int findNumbers(int[] arr) {
+        int count = 0;
+        return count;
+    }
+}
