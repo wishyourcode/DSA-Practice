dac12f027c3d9350acd5cc27605ca4b0d01399a5
wishyourcode <vishalm3101@gmail.com>
2026-02-04 21:33:51 +0530

    Hashset Fucntion

--- CodingNinja/LongestConsecutive.java
+++ CodingNinja/LongestConsecutive.java
@@ -1,5 +1,20 @@
 import java.util.HashSet;
 
+/*You are given an unsorted array/list 'ARR' of 'N' integers. 
+Your task is to return the length of the longest consecutive sequence.
+
+The consecutive sequence is in the form ['NUM', 'NUM' + 1, 'NUM' + 2, ..., 'NUM' + L]
+ where 'NUM' is the starting integer of the sequence and 'L' + 1 is the length of the sequence.
+Note:
+If there are any duplicates in the given array we will count
+ only one of them in the consecutive sequence.
+For example-
+For the given 'ARR' [9,5,4,9,10,10,6].
+Output = 3
+The longest consecutive sequence is [4,5,6].
+Follow Up:
+Can you solve this in O(N) time and O(N) 
+space complexity? */
 public class LongestConsecutive {
 
     public static int lengthOfLongestConsecutiveSequence(int[] arr, int N) {
@@ -12,7 +27,7 @@ public class LongestConsecutive {
             if (!set.contains(num - 1)) {
                 int currectnum = num;
                 int count = 1;
-                while (set.contains(currectnum)) {
+                while (set.contains(currectnum + 1)) {
                     currectnum++;
                     count++;
 
@@ -24,7 +39,7 @@ public class LongestConsecutive {
     }
 
     public static void main(String[] args) {
-        int[] arr = { 9, 5, 4, 9, 10, 10, 6 };
-        System.out.println(lengthOfLongestConsecutiveSequence(arr, 5));
+        int[] arr = { 0, 0 };
+        System.out.println(lengthOfLongestConsecutiveSequence(arr, 2));
     }
 }
