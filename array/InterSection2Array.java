e464784913a951973e95ed651a458d566690df97
wishyourcode <vishalm3101@gmail.com>
2026-02-07 23:53:22 +0530

    inter section of the 2 array result with unique element

--- array/InterSection2Array.java
+++ array/InterSection2Array.java
@@ -1,5 +1,11 @@
 package array;
 
+/* given two integer arrays, find and return the intersection of the two arrays.
+each element in the result should be unique and the result can be in any order.
+Example:    
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+*/
 public class InterSection2Array {
 
 }
