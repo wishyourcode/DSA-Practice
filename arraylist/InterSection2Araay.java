33c620d917eda6215787fdf42491c9eaca78451f
wishyourcode <vishalm3101@gmail.com>
2026-02-08 11:42:03 +0530

    Intersextion with o(n)

--- arraylist/InterSection2Araay.java
+++ arraylist/InterSection2Araay.java
@@ -3,6 +3,15 @@ package arraylist;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/* Problem statement
+You are given two arrays 'A' and 'B' of size 'N' and 'M' respectively. Both these arrays are sorted in non-decreasing order. You have to find the intersection of these two arrays.
+Intersection of two arrays is an array that consists of all the common elements occurring in both arrays.
+Note :
+1. The length of each array is greater than zero.
+2. Both the arrays are sorted in non-decreasing order.
+3. The output should be in the order of elements that occur in the original arrays.
+4. If there is no intersection present then return an empty array.
+*/
 public class InterSection2Araay {
     public static ArrayList<Integer> findArrayIntersection(ArrayList<Integer> arr1, int n, ArrayList<Integer> arr2,
             int m) {
