ea8fef089b81e726d97af246b5cee1d7c07cc637
wishyourcode <vishalm3101@gmail.com>
2026-02-14 21:48:47 +0530

    maximum sum subarray problem

--- striver_sheet/slidingwindow/MaxSumArray.java
+++ striver_sheet/slidingwindow/MaxSumArray.java
@@ -2,6 +2,12 @@ package striver_sheet.slidingwindow;
 
 import java.util.Scanner;
 
+/*Given an array of numbers, find the maximum 
+sum of any contiguous subarray of the array.
+For example, given the array [34, -50, 42, 14, -5, 86], 
+the maximum sum would be 137, since we would take elements 
+42, 14, -5, and 86.
+Given the array [-5, -1, -8, -9], the maximum sum would be -1. */
 public class MaxSumArray {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
